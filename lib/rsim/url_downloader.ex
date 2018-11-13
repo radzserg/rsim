51c0f3809ae6bd63b50f48ba4aa64c37c2b3f780
Sergey Radzishevskyi <radzserg@gmail.com>
2018-11-13 20:13:13 +0300

    Adds more use cases to UrlDownloader

--- lib/rsim/url_downloader.ex
+++ lib/rsim/url_downloader.ex
@@ -8,13 +8,17 @@ defmodule Rsim.UrlDownloader do
   Creates image from provided URL
   """
   def to_tmp_file(url) do
-    %HTTPoison.Response{body: body, headers: headers} = HTTPoison.get!(url)
-    size = fetch_header_value(headers, "Content-Length")
+    save_tmp_file(url, HTTPoison.get(url))
+  end
 
+  defp save_tmp_file(url, {:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
     tmp_path = build_tmp_path(url)
     File.write!(tmp_path, body)
-    tmp_path
+    {:ok, tmp_path}
   end
+
+  defp save_tmp_file(_, {:ok, %HTTPoison.Response{status_code: 404}}), do: {:error, :not_exists}
+  defp save_tmp_file(_, {:error, %HTTPoison.Error{reason: reason}} ), do: {:error, reason}
 
   defp build_tmp_path(url) do
     uri = URI.parse(url)
@@ -23,19 +27,5 @@ defmodule Rsim.UrlDownloader do
     tmp_dir = Path.join(System.tmp_dir(), unique_dir)
     File.mkdir_p!(tmp_dir)
     "#{tmp_dir}/#{basename}"
-  end
-
-  defp fetch_header_value(headers, needed_header) do
-    needed_header = String.downcase(needed_header)
-    header = Enum.find(headers, fn(header) ->
-      {header, _value} = header
-      String.downcase(header) == needed_header
-    end)
-    if header do
-      {_header, value} = header
-      value
-    else
-      nil
-    end
   end
 end
