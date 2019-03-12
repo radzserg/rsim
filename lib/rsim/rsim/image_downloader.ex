b194e2f62ab71396c07fb5b4fce819b5ca5e8104
Sergey Radzishevskyi <radzserg@gmail.com>
2019-03-12 13:52:35 +0300

    Rely on HTTPoison {:ok, _} when download image
    Strict check for code 200 doesn't always work. For
    example code 302 can be returned or other. So
    we will rely on HTTPoison response.

--- lib/rsim/rsim/image_downloader.ex
+++ lib/rsim/rsim/image_downloader.ex
@@ -22,14 +22,13 @@ defmodule Rsim.ImageDownloader do
     end
   end
 
-  defp save_tmp_file(url, {:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
+  defp save_tmp_file(_, {:ok, %HTTPoison.Response{status_code: 404}}), do: {:error, :not_exists}
+  defp save_tmp_file(_, {:error, %HTTPoison.Error{reason: reason}}), do: {:error, reason}
+  defp save_tmp_file(url, {:ok, %HTTPoison.Response{body: body}}) do
     tmp_path = create_tmp_path(url)
     File.write!(tmp_path, body)
     {:ok, tmp_path}
   end
-
-  defp save_tmp_file(_, {:ok, %HTTPoison.Response{status_code: 404}}), do: {:error, :not_exists}
-  defp save_tmp_file(_, {:error, %HTTPoison.Error{reason: reason}}), do: {:error, reason}
 
   defp create_tmp_path(url) do
     tmp_path = PathBuilder.tmp_path_from_url(url)
